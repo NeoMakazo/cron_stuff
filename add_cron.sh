a8638a4deeff4cd82bdebda4d4cc5ff185ea6a7f
NeoMakazo <50286382+NeoMakazo@users.noreply.github.com>
2021-03-26 12:07:54 -0400

    Create add_cron.sh
    initial script

---
+++ add_cron.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# make $tmpfile
+tmpfile=$(mktemp)
+#create directory to store the scripts
+mkdir /home/$USER/Downloads/scripts
+#ensure git is installed, and install if not
+if git --version ; then
+	git clone https://github.com/NeoMakazo/cron_stuff.git
+else
+	echo "git is not installed. Proceed?"
+	select yn in "yes" "no"; do
+		case $yn in
+			Yes ) sudo apt-get install git; break;;
+			no ) exit;;
+		esac
+	done
+# change directories to the repo
+cd cron_stuff
+#cron stuff
+crontab -l > $tmpfile
+chmod +x *
+echo "@daily cd /home/$USER/Downloads/scripts/cron_stuff && sh auto-cron.sh" >> $tmpfile
+crontab $tmpfile
+rm $tmpfile
